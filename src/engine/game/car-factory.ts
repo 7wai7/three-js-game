9f5c69563c51c8dfe954fb961f5b8ea3ca7cced8
7wai7 <nazarkalataluk@gmail.com>
2026-06-03 10:34:55 +0300

    refactoring

--- src/engine/game/car-factory.ts
+++ src/engine/game/car-factory.ts
@@ -5,34 +5,11 @@ import Object3DComponent from "../components/object";
 import RigidBodyComponent from "../components/rigidbody";
 import ColliderComponent from "../components/collider";
 
-import getObjectSize from "../../utils/get-object-size";
 import type Engine from "../engine";
 import { GROUP_PLAYER, GROUP_VEHICLE, GROUP_WHEEL, GROUP_WORLD, interactionGroups } from "./physics-groups";
 import { resolveSpawnTransform, type SpawnTransform } from "../../utils/spawn-transform";
-
-const COLLIDER_SHAPE = ["BOX", "BALL", "CAPSULE", "CYLINDER"] as const;
-type ColliderShape = typeof COLLIDER_SHAPE[number];
-
-const RIGIDBODY_DESC = ["FIXED", "DYNAMIC", "KINEMATIC"] as const;
-type RigidBodyDesc = typeof RIGIDBODY_DESC[number];
-
-const AXIS = ["X", "Y", "Z"];
-type Axis = typeof AXIS[number];
-
-type CreatedPhysicsObject = {
-    mesh: any;
-    rigidBody: RAPIER.RigidBody;
-    collider: RAPIER.Collider;
-    entity?: number
-}
-
-type ColliderDefinition = {
-    mesh: THREE.Object3D;
-    colliderMarker: THREE.Object3D;
-    shape: ColliderShape,
-    rigidBodyDesc: RigidBodyDesc,
-    axis: Axis
-};
+import { extractAndCreateCollidersByDefinitions } from "../gen-physics-by-model-markers/collider-definition";
+import { createWheelSuspensionJoint } from "../gen-physics-by-model-markers/utils";
 
 export async function createCar(
     engine: Engine,
@@ -51,35 +28,18 @@ export async function createCar(
         if (obj instanceof THREE.Mesh) {
             obj.castShadow = true;
             obj.receiveShadow = true;
-
-            if (obj.name.startsWith("COL_")) obj.visible = false;
         }
     });
 
-    scene.add(root);
 
-
-
-
-    const colliderDefinitions = extractPhysicsDefinitions(root);
-    const physics = createPhysicsFromDefinitions(
-        colliderDefinitions,
+    const physics = extractAndCreateCollidersByDefinitions(
+        root,
         physicsWorld
     )
 
     const physicsMap = new Map(
         physics.map((p) => [p.mesh.name, p]),
     );
-
-    for (const data of physics) {
-        const entity = world.createEntity();
-        const p = physicsMap.get(data.mesh.name);
-        if (p) p.entity = entity;
-
-        world.addComponent(entity, new Object3DComponent(data.mesh));
-        world.addComponent(entity, new ColliderComponent(data.collider));
-        world.addComponent(entity, new RigidBodyComponent(data.rigidBody));
-    }
 
     const chassis = physicsMap.get("Chassis")!;
     const fl = physicsMap.get("FL")!;
@@ -90,9 +50,20 @@ export async function createCar(
     const wheels = [fl, fr, bl, br];
 
 
+    const chassisEntity = world.createEntity();
+    world.addComponent(chassisEntity, new Object3DComponent(chassis.mesh));
+    world.addComponent(chassisEntity, new ColliderComponent(chassis.collider));
+    world.addComponent(chassisEntity, new RigidBodyComponent(chassis.rigidBody));
 
     for (const w of wheels) {
-        createWheelJoint(
+        const entity = world.createEntity();
+        world.addComponent(entity, new Object3DComponent(w.mesh));
+        world.addComponent(entity, new ColliderComponent(w.collider));
+        world.addComponent(entity, new RigidBodyComponent(w.rigidBody));
+    }
+
+    for (const w of wheels) {
+        createWheelSuspensionJoint(
             physicsWorld,
             chassis.rigidBody,
             w.rigidBody,
@@ -135,286 +106,7 @@ export async function createCar(
             );
     }
 
-    return chassis.entity
-}
 
-
-// utils
-function extractPhysicsDefinitions(root: THREE.Object3D) {
-    const result: ColliderDefinition[] = [];
-
-    root.traverse((obj) => {
-        if (!obj.name.startsWith("COL_")) return;
-        if (!obj.parent) return;
-
-        const parts = obj.name.split("_");
-
-        const shape = COLLIDER_SHAPE.find(shape => obj.name.includes(shape)) ?? "BOX";
-        const rigidBodyDesc = RIGIDBODY_DESC.find(desc => obj.name.includes(desc)) ?? "DYNAMIC";
-
-        const axis =
-            parts.find(
-                (part): part is Axis =>
-                    AXIS.includes(part as Axis),
-            ) ?? "Y";
-
-        result.push({
-            mesh: obj.parent,
-            colliderMarker: obj,
-            shape,
-            rigidBodyDesc,
-            axis
-        });
-    });
-
-    return result;
-}
-
-function createPhysicsFromDefinitions(
-    defs: ColliderDefinition[],
-    physicsWorld: RAPIER.World,
-): CreatedPhysicsObject[] {
-    const result = [];
-
-    for (const def of defs) {
-        const meshWorldPos = new THREE.Vector3();
-        const meshWorldQuat = new THREE.Quaternion();
-
-        def.mesh.getWorldPosition(meshWorldPos);
-        def.mesh.getWorldQuaternion(meshWorldQuat);
-
-        const size = getObjectSize(def.colliderMarker);
-        console.log(`Shape: ${def.shape}, desc: ${def.rigidBodyDesc}, axis: ${def.axis}, size: ${size.toArray()}`)
-
-        let rbDesc: RAPIER.RigidBodyDesc;
-
-        switch (def.rigidBodyDesc) {
-            case "FIXED":
-                rbDesc = RAPIER.RigidBodyDesc.fixed();
-                break;
-
-            case "KINEMATIC":
-                rbDesc = RAPIER.RigidBodyDesc.kinematicPositionBased();
-                break;
-
-            default:
-                rbDesc = RAPIER.RigidBodyDesc.dynamic();
-                break;
-        }
-
-        rbDesc.setTranslation(
-            meshWorldPos.x,
-            meshWorldPos.y,
-            meshWorldPos.z,
-        );
-
-        rbDesc.setRotation({
-            x: meshWorldQuat.x,
-            y: meshWorldQuat.y,
-            z: meshWorldQuat.z,
-            w: meshWorldQuat.w,
-        });
-
-        const rb = physicsWorld.createRigidBody(rbDesc);
-
-        let colliderDesc: RAPIER.ColliderDesc;
-        const { length, radius } =
-            getAxisDimensions(size, def.axis.toLowerCase() as "x" | "y" | "z");
-
-        switch (def.shape) {
-            case "BALL": {
-                const radius =
-                    Math.max(size.x, size.y, size.z) * 0.5;
-
-                colliderDesc =
-                    RAPIER.ColliderDesc.ball(radius);
-
-                break;
-            }
-
-            case "CAPSULE": {
-                const halfHeight =
-                    Math.max(0, length * 0.5 - radius);
-
-                colliderDesc =
-                    RAPIER.ColliderDesc.capsule(
-                        halfHeight,
-                        radius,
-                    );
-
-                break;
-            }
-
-            case "CYLINDER": {
-                colliderDesc =
-                    RAPIER.ColliderDesc.cylinder(
-                        length * 0.5,
-                        radius,
-                    );
-
-                break;
-            }
-
-            default: {
-                colliderDesc =
-                    RAPIER.ColliderDesc.cuboid(
-                        size.x * 0.5,
-                        size.y * 0.5,
-                        size.z * 0.5,
-                    );
-
-                break;
-            }
-        }
-
-        const localPos = new THREE.Vector3();
-
-        def.mesh.updateMatrixWorld(true);
-        def.colliderMarker.updateMatrixWorld(true);
-
-        const inverseMeshMatrix =
-            def.mesh.matrixWorld.clone().invert();
-
-        const localMatrix =
-            inverseMeshMatrix.multiply(
-                def.colliderMarker.matrixWorld.clone(),
-            );
-
-        localMatrix.decompose(
-            localPos,
-            new THREE.Quaternion(),
-            new THREE.Vector3(),
-        );
-
-        colliderDesc.setTranslation(
-            localPos.x,
-            localPos.y,
-            localPos.z,
-        );
-
-
-
-
-        const localQuat = getColliderRotation(def.axis);
-
-        colliderDesc.setRotation({
-            x: localQuat.x,
-            y: localQuat.y,
-            z: localQuat.z,
-            w: localQuat.w,
-        });
-
-        const collider = physicsWorld.createCollider(
-            colliderDesc,
-            rb,
-        );
-
-        result.push({
-            mesh: def.mesh,
-            rigidBody: rb,
-            collider,
-        });
-    }
-
-    return result;
-}
-
-function getColliderRotation(axis: Axis) {
-    switch (axis) {
-        case "X":
-            return new THREE.Quaternion().setFromEuler(
-                new THREE.Euler(0, 0, Math.PI / 2),
-            );
-
-        case "Z":
-            return new THREE.Quaternion().setFromEuler(
-                new THREE.Euler(Math.PI / 2, 0, 0),
-            );
-
-        default:
-            return new THREE.Quaternion();
-    }
-}
-
-function getAxisDimensions(
-    size: THREE.Vector3,
-    axis: "x" | "y" | "z",
-) {
-    switch (axis) {
-        case "x":
-            return {
-                length: size.x,
-                radius: Math.max(size.y, size.z) * 0.5,
-            };
-
-        case "y":
-            return {
-                length: size.y,
-                radius: Math.max(size.x, size.z) * 0.5,
-            };
-
-        case "z":
-            return {
-                length: size.z,
-                radius: Math.max(size.x, size.y) * 0.5,
-            };
-    }
-}
-
-function createWheelJoint(
-    physicsWorld: RAPIER.World,
-    chassis: RAPIER.RigidBody,
-    wheel: RAPIER.RigidBody,
-) {
-    const chassisPos = chassis.translation();
-    const wheelPos = wheel.translation();
-
-    const anchor1 = {
-        x: wheelPos.x - chassisPos.x,
-        y: wheelPos.y - chassisPos.y,
-        z: wheelPos.z - chassisPos.z,
-    };
-
-    const anchor2 = {
-        x: 0,
-        y: 0,
-        z: 0,
-    };
-
-    const axis = {
-        x: 0,
-        y: 1,
-        z: 0,
-    };
-
-    const joint = RAPIER.JointData.prismatic(
-        anchor1,
-        anchor2,
-        axis,
-    );
-
-    const j = physicsWorld.createImpulseJoint(
-        joint,
-        chassis,
-        wheel,
-        true,
-    ) as RAPIER.PrismaticImpulseJoint;
-
-    j.setLimits(
-        -0.25,
-        0.25,
-    );
-
-    j.configureMotorPosition(
-        -0.25,
-        300,
-        40,
-    );
-
-    // wheel.setEnabledRotations(
-    //     false,
-    //     true,
-    //     false,
-    //     true,
-    // );
+    scene.add(root);
+    return chassisEntity
 }
